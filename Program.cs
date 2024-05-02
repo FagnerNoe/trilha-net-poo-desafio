ebe96fc9f0985862e50f6c71c46dae577ef5b20e
Fagner <fagnernezim@gmail.com>
2024-05-01 21:56:57 -0300

    implementadas as classes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// TODO: Realizar os testes com as classes Nokia e Ipho
+Console.WriteLine("Nokia");
+Smartphone nokia = new Nokia(numero: "14253678", modelo: "Modelo C-90", imei: "34567876", memoria: 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Iphone");
+Smartphone iphone = new Iphone(numero: "456789", modelo: "Iphone 12", imei: "5678473", memoria: 512);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
